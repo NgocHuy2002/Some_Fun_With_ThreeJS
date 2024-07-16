ca9e6994435e721e890464063b83de914470a709
DAONGOCHUY <daongochuy@vnpt.vn>
2024-07-16 16:27:39 +0700

    add model

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Html, useProgress } from '@react-three/drei'
+import { Suspense } from "react";
 
-function App() {
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./scifi_girl_v.01/scene.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={5} />
+    </>
+  );
+};
+
+const Tree = () => {
+  const gltf = useLoader(GLTFLoader, "./voxel_tutorial_-_scene_2/scene.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.4} />
+    </>
+  );
+};
+function Loader() {
+  const { progress } = useProgress()
+  return <Html center>{progress} % loaded</Html>
+}
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <Suspense fallback={<Loader />}>
+          <Tree />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
-
-export default App;
